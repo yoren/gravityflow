3be1c9678947b0563eec043066067b426a099ecf
Steve Henty <stevehenty@gmail.com>
2016-02-07 11:11:06 +0100

    removing instructions box on submit

--- class-gravity-flow.php
+++ class-gravity-flow.php
@@ -2904,6 +2904,8 @@ PRIMARY KEY  (id)
 					$current_user_assignee_key = $this->get_current_user_assignee_key();
 					if ( ( $next_step && $next_step->is_assignee( $current_user_assignee_key ) ) || $args['check_permissions'] == false || $this->current_user_can_any( 'gravityflow_view_all' ) ) {
 						$step = $next_step;
+					} else {
+						$args['display_instructions'] = false;
 					}
 					$args['check_permissions'] = false;
 				}
