d46dfee35239631c12d5208359674f989d30728c
Steve Henty <stevehenty@gmail.com>
2016-02-14 22:06:26 +0100

    adding docblocks

--- includes/pages/class-entry-editor.php
+++ includes/pages/class-entry-editor.php
@@ -4,17 +4,49 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Gravity Flow Entry Editor
+ *
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Gravity_Flow_Entry_Editor
+ * @copyright   Copyright (c) 2015-2016, Steven Henty
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.2.0.30
+ */
 class Gravity_Flow_Entry_Editor {
 
+	/**
+	 * The Gravity Forms form array.
+	 *
+	 * @var array
+	 */
 	public $form;
 
+	/**
+	 * The Gravity Forms Entry array.
+	 * @var
+	 */
 	public $entry;
 
-	/* @var Gravity_Flow_Step $step */
+	/**
+	 *
+	 * @var Gravity_Flow_Step $step
+	 */
 	public $step;
 
+	/**
+	 * Used to help determine whether to display the order summary.
+	 *
+	 * @var bool
+	 */
 	public $has_product_fields = false;
 
+	/**
+	 * Flag set in the constructor to control the visibility of empty fields.
+	 *
+	 * @var
+	 */
 	public $display_empty_fields;
 
 	public function __construct( $form, $entry, $step, $display_empty_fields ) {
@@ -25,6 +57,10 @@ class Gravity_Flow_Entry_Editor {
 		$this->display_empty_fields = $display_empty_fields;
 	}
 
+
+	/**
+	 * Renders the form. Uses GFFormDisplay::get_form() to display the fields.
+	 */
 	public function render_edit_form() {
 
 		add_filter( 'gform_pre_render', array( $this, 'filter_gform_pre_render' ), 999, 3 );
@@ -61,6 +97,14 @@ class Gravity_Flow_Entry_Editor {
 		echo $html;
 	}
 
+	/**
+	 * Target of the gform_pre_render filter.
+	 * Removes the page fields from the form.
+	 *
+	 * @param array $form
+	 *
+	 * @return array the filtered form
+	 */
 	public function filter_gform_pre_render( $form ) {
 		// Remove page fields
 		$existing_fields = $form['fields'];
@@ -80,11 +124,23 @@ class Gravity_Flow_Entry_Editor {
 		return $form;
 	}
 
+	/**
+	 * Target for the gform_submit_button filter.
+	 *
+	 * @param $button_input
+	 * @param $form
+	 *
+	 * @return string
+	 */
 	public function filter_gform_submit_button( $button_input, $form ) {
 		return '';
 	}
 
 	/**
+	 * Taget for the gform_field_input filter.
+	 *
+	 * Handles the construction of the field input. Returns markup for the editable field or the display value.
+	 *
 	 * @param $html
 	 * @param GF_Field $field
 	 * @param $value
@@ -132,25 +188,53 @@ class Gravity_Flow_Entry_Editor {
 		return $html;
 	}
 
+	/**
+	 * Checks wether dynamic conditional logic is enabled.
+	 *
+	 * @return bool
+	 */
 	public function is_dynamic_conditional_logic_enabled() {
 		return $this->step && gravity_flow()->fields_have_conditional_logic( $this->form ) && $this->step->conditional_logic_editable_fields_enabled && $this->step->conditional_logic_editable_fields_mode != 'page_load';
 	}
 
+	/**
+	 * Target for the gform_get_form_filter filter.
+	 * Strips the closing form tag and replaces the Gravity Forms token for Gravity Flow's token.
+	 *
+	 * @param $form_string
+	 * @param $form
+	 *
+	 * @return mixed
+	 */
 	public function filter_gform_get_form_filter( $form_string, $form ) {
 		$form_string = str_replace( 'gform_submit', 'gravityflow_submit', $form_string );
 		$form_string = str_replace( '</form>', '', $form_string );
 		return $form_string;
 	}
 
+	/**
+	 * Target for the gform_form_tag filter.
+	 *
+	 * Strips the form tag off.
+	 *
+	 * @param $form_tag
+	 * @param $form
+	 *
+	 * @return string
+	 */
 	public function filter_gform_form_tag( $form_tag, $form ) {
 		return '';
 	}
 
 	/**
+	 * Generates and returns the markup for a display field.
+	 *
 	 * @param GF_Field $field
 	 * @param $value
 	 * @param $lead_id
 	 * @param $form_id
+	 *
+	 * @return string
 	 */
 	public function get_display_field( $field, $value, $lead_id, $form_id ) {
 
@@ -230,6 +314,13 @@ class Gravity_Flow_Entry_Editor {
 		return $html;
 	}
 
+	/**
+	 * Checks whether the given field is a display field and whether it should be displayed.
+	 *
+	 * @param $field
+	 *
+	 * @return bool
+	 */
 	public function is_display_field( $field ) {
 		$display_field = true;
 
@@ -250,10 +341,31 @@ class Gravity_Flow_Entry_Editor {
 		return $display_field;
 	}
 
+	/**
+	 * Checks whether a field is an editable field.
+	 *
+	 * @param $field
+	 *
+	 * @return bool
+	 */
 	public function is_editable_field( $field ) {
 		return in_array( $field->id, $this->step->get_editable_fields() );
 	}
 
+
+	/**
+	 * Target for the gform_field_content filter.
+	 *
+	 * Checks whether the field should hidden and then ensures that the markup is completely empty for the field.
+	 *
+	 * @param $content
+	 * @param $field
+	 * @param $value
+	 * @param $lead_id
+	 * @param $form_id
+	 *
+	 * @return string
+	 */
 	public function filter_gform_field_content( $content, $field, $value, $lead_id, $form_id ) {
 
 		$hidden_token = '<!-- gravityflow_hidden -->';
@@ -273,6 +385,15 @@ class Gravity_Flow_Entry_Editor {
 		return $content;
 	}
 
+	/**
+	 * Checks whether the section should be hidden for the given section field.
+	 *
+	 * Hidden sections contain no editable fields and no non-empty display fields.
+	 *
+	 * @param $section_field
+	 *
+	 * @return bool
+	 */
 	public function is_section_hidden( $section_field ) {
 
 		$has_an_editable_field = false;
@@ -291,6 +412,20 @@ class Gravity_Flow_Entry_Editor {
 		return GFCommon::is_section_empty( $section_field, $this->form, $this->entry ) && ! $this->display_empty_fields;
 	}
 
+	/**
+	 * Target for the gform_field_container filter.
+	 *
+	 * Removes the markup completely for section fields that are hidden.
+	 *
+	 * @param $field_container
+	 * @param $field
+	 * @param $form
+	 * @param $css_class
+	 * @param $style
+	 * @param $field_content
+	 *
+	 * @return string
+	 */
 	public function filter_gform_field_container( $field_container, $field, $form, $css_class, $style, $field_content ) {
 		if ( $field->type == 'section' && $this->is_section_hidden( $field ) ) {
 			$field_container = '';
@@ -299,11 +434,29 @@ class Gravity_Flow_Entry_Editor {
 		return $field_container;
 	}
 
+	/**
+	 * Target for the gform_has_conditional_logic filter.
+	 *
+	 * Checks the condtional logic setting and configures the form accordingly.
+	 *
+	 * @return bool
+	 */
 	public function filter_gform_has_conditional_logic() {
 		$dynamic_conditional_logic_enabled = $this->is_dynamic_conditional_logic_enabled();
 		return $dynamic_conditional_logic_enabled;
 	}
 
+	/**
+	 * Target for the gform_field_css_class filter.
+	 *
+	 * Checks the step settings and adds the appropriate classes.
+	 *
+	 * @param $classes
+	 * @param $field
+	 * @param $form
+	 *
+	 * @return string
+	 */
 	public function filter_gform_field_css_class( $classes, $field, $form ) {
 		$is_editable = $this->is_editable_field( $field );
 		$editable_class = $is_editable ? 'gravityflow-editable-field' : '';
