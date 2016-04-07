42776bac0f7d0819fd1e107ecca7f542880fc848
richardW8k <richard@wawrzyniak.me>
2016-04-07 20:27:46 +0100

    Freshbooks Step

---
+++ includes/steps/class-step-feed-freshbooks.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Gravity Flow Step Feed FreshBooks
+ *
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Gravity_Flow_Step_Feed_FreshBooks
+ * @copyright   Copyright (c) 2016, Steven Henty
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.3.1.6
+ */
+
+if ( ! class_exists( 'GFForms' ) ) {
+	die();
+}
+
+class Gravity_Flow_Step_Feed_FreshBooks extends Gravity_Flow_Step_Feed_Add_On {
+	public $_step_type = 'freshbooks';
+
+	protected $_class_name = 'GFFreshBooks';
+
+	public function get_label() {
+		return esc_html__( 'FreshBooks', 'gravityflow' );
+	}
+
+	function get_feed_label( $feed ) {
+		$label = $feed['meta']['feedName'];
+		return $label;
+	}
+}
+Gravity_Flow_Steps::register( new Gravity_Flow_Step_Feed_FreshBooks() );
