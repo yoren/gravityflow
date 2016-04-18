7c6dac885a7f03d31f86fa866799fac87da9cf82
richardW8k <richard@wawrzyniak.me>
2016-04-18 16:08:36 +0100

    Highrise Step

---
+++ includes/steps/class-step-feed-highrise.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Gravity Flow Step Feed Highrise
+ *
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Gravity_Flow_Step_Feed_Highrise
+ * @copyright   Copyright (c) 2016, Steven Henty
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.3.2-dev
+ */
+
+if ( ! class_exists( 'GFForms' ) ) {
+	die();
+}
+
+class Gravity_Flow_Step_Feed_Highrise extends Gravity_Flow_Step_Feed_Add_On {
+	public $_step_type = 'highrise';
+
+	protected $_class_name = 'GFHighrise';
+
+	public function get_label() {
+		return esc_html__( 'Highrise', 'gravityflow' );
+	}
+
+	function get_feed_label( $feed ) {
+		$label = $feed['meta']['feed_name'];
+		return $label;
+	}
+}
+Gravity_Flow_Steps::register( new Gravity_Flow_Step_Feed_Highrise() );
