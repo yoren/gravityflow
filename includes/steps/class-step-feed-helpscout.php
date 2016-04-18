4eb9a6f9d8c04ea1a3ee549fb97ebbd5329b54d8
richardW8k <richard@wawrzyniak.me>
2016-04-18 16:08:08 +0100

    Help Scout Step

---
+++ includes/steps/class-step-feed-helpscout.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Gravity Flow Step Feed Help Scout
+ *
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Gravity_Flow_Step_Feed_HelpScout
+ * @copyright   Copyright (c) 2016, Steven Henty
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.3.2-dev
+ */
+
+if ( ! class_exists( 'GFForms' ) ) {
+	die();
+}
+
+class Gravity_Flow_Step_Feed_HelpScout extends Gravity_Flow_Step_Feed_Add_On {
+	public $_step_type = 'helpscout';
+
+	protected $_class_name = 'GFHelpScout';
+
+	public function get_label() {
+		return esc_html__( 'Help Scout', 'gravityflow' );
+	}
+
+	function get_feed_label( $feed ) {
+		$label = $feed['meta']['feed_name'];
+		return $label;
+	}
+}
+Gravity_Flow_Steps::register( new Gravity_Flow_Step_Feed_HelpScout() );
