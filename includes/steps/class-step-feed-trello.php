560fb6c80085b3ed13b0c0372e574f315a3a1462
richardW8k <richard@wawrzyniak.me>
2016-04-30 11:23:21 +0100

    Trello Step

---
+++ includes/steps/class-step-feed-trello.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Gravity Flow Step Feed Trello
+ *
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Gravity_Flow_Step_Feed_Trello
+ * @copyright   Copyright (c) 2016, Steven Henty
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.3.2-dev
+ */
+
+if ( ! class_exists( 'GFForms' ) ) {
+	die();
+}
+
+class Gravity_Flow_Step_Feed_Trello extends Gravity_Flow_Step_Feed_Add_On {
+	public $_step_type = 'trello';
+
+	protected $_class_name = 'GFTrello';
+
+	public function get_label() {
+		return esc_html__( 'Trello', 'gravityflow' );
+	}
+
+	function get_feed_label( $feed ) {
+		$label = $feed['meta']['feedName'];
+		return $label;
+	}
+
+	public function get_icon_url() {
+		return $this->get_base_url() . '/images/trello-icon.svg';
+	}
+}
+Gravity_Flow_Steps::register( new Gravity_Flow_Step_Feed_Trello() );
