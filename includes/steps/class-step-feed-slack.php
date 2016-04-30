88c678e1be5b2503c2995b48ad56dc9bd53013d2
richardW8k <richard@wawrzyniak.me>
2016-04-30 11:10:15 +0100

    Slack Step

---
+++ includes/steps/class-step-feed-slack.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Gravity Flow Step Feed Slack
+ *
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Gravity_Flow_Step_Feed_Slack
+ * @copyright   Copyright (c) 2016, Steven Henty
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.3.2-dev
+ */
+
+if ( ! class_exists( 'GFForms' ) ) {
+	die();
+}
+
+class Gravity_Flow_Step_Feed_Slack extends Gravity_Flow_Step_Feed_Add_On {
+	public $_step_type = 'slack';
+
+	protected $_class_name = 'GFSlack';
+
+	public function get_label() {
+		return esc_html__( 'Slack', 'gravityflow' );
+	}
+
+	function get_feed_label( $feed ) {
+		$label = $feed['meta']['feed_name'];
+		return $label;
+	}
+
+	public function get_icon_url() {
+		return $this->get_base_url() . '/images/slack-icon.png';
+	}
+}
+Gravity_Flow_Steps::register( new Gravity_Flow_Step_Feed_Slack() );
