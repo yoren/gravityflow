0753ce3623797cbe9a22298b3e9486b79a881f24
richardW8k <richard@wawrzyniak.me>
2016-06-01 14:52:38 +0100

    More WP E-Signature
    Step status is now pending until document has been signed.

--- includes/steps/class-step-feed-wp-e-signature.php
+++ includes/steps/class-step-feed-wp-e-signature.php
@@ -19,6 +19,8 @@ class Gravity_Flow_Step_Feed_Esign extends Gravity_Flow_Step_Feed_Add_On {
 
 	protected $_class_name = 'GFEsignAddOn';
 	protected $_slug = 'esig-gf';
+	protected $_feed_id;
+
 
 	public function get_label() {
 		return esc_html__( 'WP E-Signature', 'gravityflow' );
@@ -52,6 +54,8 @@ class Gravity_Flow_Step_Feed_Esign extends Gravity_Flow_Step_Feed_Add_On {
 			$feed['meta']['esign_gf_logic'] = 'email';
 		}
 
+		$this->_feed_id = $feed['id'];
+		add_action( 'esig_sad_document_invite_send', array( $this, 'sad_document_invite_send' ) );
 		parent::process_feed( $feed );
 
 		// only perform the redirect when the previous step type was user_input
@@ -101,5 +105,78 @@ class Gravity_Flow_Step_Feed_Esign extends Gravity_Flow_Step_Feed_Add_On {
 		return $previous_step;
 	}
 
+	/**
+	 * Evaluates the status for the step.
+	 *
+	 * The step is only complete after gravity_flow_step_esign_document_complete() has added all the feeds for this step back into the entry meta processed_feeds array.
+	 *
+	 * @return string 'pending' or 'complete'
+	 */
+	public function evaluate_status() {
+		$status = $this->get_status();
+
+		if ( empty( $status ) ) {
+			return 'pending';
+		}
+
+		if ( $status == 'pending' ) {
+			$add_on_feeds = $this->get_processed_add_on_feeds();
+			$feeds        = $this->get_feeds();
+			foreach ( $feeds as $feed ) {
+				$setting_key = 'feed_' . $feed['id'];
+				if ( $this->{$setting_key} && ! in_array( $feed['id'], $add_on_feeds ) ) {
+					return 'pending';
+				}
+			}
+		}
+
+		return 'complete';
+	}
+
+	/**
+	 * Target of esig_sad_document_invite_send hook. Store the feed id which created this document in the WP E-Signature meta.
+	 *
+	 * @param array $args The properties related to the document which was saved.
+	 */
+	public function sad_document_invite_send( $args ) {
+		if ( ! empty( $this->_feed_id ) && class_exists( 'WP_E_Meta' ) ) {
+			$sig_meta_api = new WP_E_Meta();
+			$sig_meta_api->add( $args['document']->document_id, 'esig_gravity_feed_id', $this->_feed_id );
+		}
+	}
+
 }
 Gravity_Flow_Steps::register( new Gravity_Flow_Step_Feed_Esign() );
+
+/**
+ * Resume the workflow if the completed document originated from a feed processed by one of our steps.
+ *
+ * @param array $args The properties related to the completed document.
+ */
+function gravity_flow_step_esign_document_complete( $args ) {
+	if ( class_exists( 'WP_E_Meta' ) ) {
+		$sig_meta_api = new WP_E_Meta();
+		$entry_id     = $sig_meta_api->get( $args['invitation']->document_id, 'esig_gravity_entry_id' );
+		$feed_id      = $sig_meta_api->get( $args['invitation']->document_id, 'esig_gravity_feed_id' );
+
+		if ( $entry_id && $feed_id ) {
+			$entry = GFAPI::get_entry( $entry_id );
+
+			if ( ! is_wp_error( $entry ) && is_array( $entry ) && rgar( $entry, 'workflow_final_status' ) == 'pending' ) {
+				$step = Gravity_Flow_Steps::get( 'wp-e-signature' );
+				if ( $step ) {
+					$add_on_feeds = $step->get_processed_add_on_feeds( $entry_id );
+
+					if ( ! in_array( $feed_id, $add_on_feeds ) ) {
+						$add_on_feeds[] = $feed_id;
+						$step->update_processed_feeds( $add_on_feeds, $entry_id );
+						$form = GFAPI::get_form( $entry['form_id'] );
+						gravity_flow()->process_workflow( $form, $entry_id );
+					}
+				}
+			}
+
+		}
+	}
+}
+add_action( 'esig_document_complate', 'gravity_flow_step_esign_document_complete' );
