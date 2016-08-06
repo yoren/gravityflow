9eede9f5a4b19d978d78d16b590f04d71b567c0d
richardW8k <richard@wawrzyniak.me>
2016-08-06 19:48:59 +0100

    Gravity_Flow_Installation_Wizard
    Working on the scrutinizer score

--- includes/wizard/class-installation-wizard.php
+++ includes/wizard/class-installation-wizard.php
@@ -38,11 +38,79 @@ class Gravity_Flow_Installation_Wizard {
 
 	public function display() {
 
-		$name = rgpost( '_step_name' );
+		/**
+		 * @var Gravity_Flow_Installation_Wizard_Step $current_step The step being displayed.
+		 * @var string $nonce_key The nonce key for the current step.
+		 */
+		list( $current_step, $nonce_key ) = $this->get_current_step();
+		$this->include_styles();
 
+		?>
+
+		<div class="wrap about-wrap gform_installation_progress_step_wrap">
+
+			<img src="<?php echo gravity_flow()->get_base_url() ?>/images/gravityflow-logo-blue-450.png"/>
+
+			<div id="gform_installation_progress">
+				<?php $this->progress( $current_step ); ?>
+			</div>
+
+			<hr/>
+
+			<br/>
+			<h2>
+				<?php echo $current_step->get_title(); ?>
+			</h2>
+
+			<form action="" method="POST">
+				<input type="hidden" name="_step_name" value="<?php echo esc_attr( $current_step->get_name() ); ?>"/>
+				<?php
+				wp_nonce_field( $nonce_key, $nonce_key );
+
+				$validation_summary = $current_step->get_validation_summary();
+				if ( $validation_summary ) {
+					printf( '<div class="delete-alert alert_red">%s</div>', $validation_summary );
+				}
+
+				?>
+				<div class="about-text">
+					<?php $current_step->display(); ?>
+				</div>
+				<?php
+				$next_button = '';
+				if ( $current_step->is( 'updates' ) ) {
+					$next_button = sprintf( '<input class="button button-primary" type="submit" value="%s" name="_install"/>', esc_attr( $current_step->get_next_button_text() ) );
+				} elseif ( ! $current_step->is( 'complete' ) ) {
+					$next_button = sprintf( '<input class="button button-primary" type="submit" value="%s" name="_next"/>', esc_attr( $current_step->get_next_button_text() ) );
+				}
+				?>
+				<div>
+					<?php
+					$previous_button_text = $current_step->get_previous_button_text();
+					if ( $previous_button_text ) {
+						$previous_button = $this->get_step_index( $current_step ) > 0 ? '<input name="_previous" class="button button-primary" type="submit" value="' . esc_attr( $previous_button_text ) . '" style="margin-right:30px;" />' : '';
+						echo $previous_button;
+					}
+					echo $next_button;
+					?>
+				</div>
+			</form>
+		</div>
+
+		<?php
+
+		return true;
+	}
+
+	/**
+	 * Get the step to be displayed and it's nonce key.
+	 *
+	 * @return array
+	 */
+	public function get_current_step() {
+		$name         = rgpost( '_step_name' );
 		$current_step = $this->get_step( $name );
-
-		$nonce_key = '_gform_installation_wizard_step_' . $current_step->get_name();
+		$nonce_key    = '_gform_installation_wizard_step_' . $current_step->get_name();
 
 		if ( isset( $_POST[ $nonce_key ] ) && check_admin_referer( $nonce_key, $nonce_key ) ) {
 
@@ -77,103 +145,49 @@ class Gravity_Flow_Installation_Wizard {
 			}
 
 			$nonce_key = '_gform_installation_wizard_step_' . $current_step->get_name();
-
 		}
 
+		return array( $current_step, $nonce_key );
+	}
+
+	/**
+	 * Registers the admin styles and includes the inline style block.
+	 */
+	public function include_styles() {
 		$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG || isset( $_GET['gform_debug'] ) ? '' : '.min';
 
 		// register admin styles
 		wp_register_style( 'gform_admin', GFCommon::get_base_url() . "/css/admin{$min}.css" );
 		wp_print_styles( array( 'jquery-ui-styles', 'gform_admin' ) );
-
 		?>
-
 		<style>
-
 			#gform_installation_progress li {
 				display: inline-block;
 				padding: 10px 20px 10px 0;
 			}
 
 			.gform_installation_progress_current_step {
-
 				color: black;
 			}
 
-			.gform_installation_progress_step_pending{
+			.gform_installation_progress_step_pending {
 				color: silver;
 			}
 
-			.gform_installation_progress_step_complete{
+			.gform_installation_progress_step_complete {
 				color: black;
 			}
 
-			.gform_installation_progress_step_wrap p{
+			.gform_installation_progress_step_wrap p {
 				color: black;
 			}
 
-			.about-text input.regular-text{
-				font-size:19px;
+			.about-text input.regular-text {
+				font-size: 19px;
 				padding: 8px;
 			}
 		</style>
-
-		<div class="wrap about-wrap gform_installation_progress_step_wrap">
-
-		<img src="<?php echo gravity_flow()->get_base_url() ?>/images/gravityflow-logo-blue-450.png" />
-
-		<div id="gform_installation_progress">
-			<?php $this->progress( $current_step ); ?>
-		</div>
-
-		<hr/>
-
-		<br />
-		<h2>
-			<?php echo $current_step->get_title(); ?>
-		</h2>
-
-			<form action="" method="POST">
-				<input type="hidden" name="_step_name" value="<?php echo esc_attr( $current_step->get_name() ); ?>"/>
-				<?php
-				wp_nonce_field( $nonce_key, $nonce_key );
-
-				$validation_summary = $current_step->get_validation_summary();
-				if ( $validation_summary ) {
-					printf( '<div class="delete-alert alert_red">%s</div>', $validation_summary );
-				}
-
-				?>
-				<div class="about-text">
-					<?php
-
-					$current_step->display( $current_step );
-					?>
-					</div>
-					<?php
-					$next_button = '';
-					if ( $current_step->is( 'updates' ) ) {
-						$next_button = sprintf( '<input class="button button-primary" type="submit" value="%s" name="_install"/>', esc_attr( $current_step->get_next_button_text() ) );
-					} elseif ( ! $current_step->is( 'complete' ) ) {
-						$next_button = sprintf( '<input class="button button-primary" type="submit" value="%s" name="_next"/>', esc_attr( $current_step->get_next_button_text() ) );
-					}
-					?>
-				<div>
-					<?php
-					$previous_button_text = $current_step->get_previous_button_text();
-					if ( $previous_button_text ) {
-						$previous_button = $this->get_step_index( $current_step ) > 0 ? '<input name="_previous" class="button button-primary" type="submit" value="' . esc_attr( $previous_button_text ) . '" style="margin-right:30px;" />' : '';
-						echo $previous_button;
-					}
-					echo $next_button;
-					?>
-				</div>
-			</form>
-		</div>
-
-	<?php
-
-		return true;
+		<?php
 	}
 
 	/**
@@ -185,12 +199,12 @@ class Gravity_Flow_Installation_Wizard {
 
 		if ( empty( $name ) ) {
 			$class_names = array_keys( $this->_step_class_names );
-			$name = $class_names[0];
+			$name        = $class_names[0];
 		}
 
 		$current_step_values = get_option( 'gravityflow_installation_wizard_' . $name );
 
-		$step = new $this->_step_class_names[ $name ]( $current_step_values );
+		$step = new $this->_step_class_names[$name]( $current_step_values );
 
 		return $step;
 	}
@@ -204,7 +218,7 @@ class Gravity_Flow_Installation_Wizard {
 		$current_step_name = $current_step->get_name();
 
 		$step_names = array_keys( $this->_step_class_names );
-		$i = array_search( $current_step_name, $step_names );
+		$i          = array_search( $current_step_name, $step_names );
 
 		if ( $i == 0 ) {
 			return false;
@@ -224,7 +238,7 @@ class Gravity_Flow_Installation_Wizard {
 		$current_step_name = $current_step->get_name();
 
 		$step_names = array_keys( $this->_step_class_names );
-		$i = array_search( $current_step_name, $step_names );
+		$i          = array_search( $current_step_name, $step_names );
 
 		if ( $i == count( $step_names ) - 1 ) {
 			return false;
@@ -263,19 +277,19 @@ class Gravity_Flow_Installation_Wizard {
 	 * @return string
 	 */
 	public function progress( $current_step, $echo = true ) {
-		$html = '<ul id="gform_installation_progress">';
-		$done = true;
+		$html              = '<ul id="gform_installation_progress">';
+		$done              = true;
 		$current_step_name = $current_step->get_name();
 		foreach ( array_keys( $this->_step_class_names ) as $step_name ) {
 			$class = '';
-			$step = $this->get_step( $step_name );
+			$step  = $this->get_step( $step_name );
 			if ( $current_step_name == $step_name ) {
 				$class .= 'gform_installation_progress_current_step ';
-				$done = $step->is('complete') ? true : false;
+				$done = $step->is( 'complete' ) ? true : false;
 			} else {
-				$class .= $done  ?  'gform_installation_progress_step_complete' : 'gform_installation_progress_step_pending';
+				$class .= $done ? 'gform_installation_progress_step_complete' : 'gform_installation_progress_step_pending';
 			}
-			$check = $done ? '<i class="fa fa-check" style="color:green"></i>'  : '<i class="fa fa-check" style="visibility:hidden"></i>';
+			$check = $done ? '<i class="fa fa-check" style="color:green"></i>' : '<i class="fa fa-check" style="visibility:hidden"></i>';
 
 			$html .= sprintf( '<li id="gform_installation_progress_%s" class="%s">%s&nbsp;%s</li>', esc_attr( $step->get_name() ), esc_attr( $class ), esc_html( $step->get_title() ), $check );
 		}
@@ -284,6 +298,7 @@ class Gravity_Flow_Installation_Wizard {
 		if ( $echo ) {
 			echo $html;
 		}
+
 		return $html;
 	}
 
@@ -294,6 +309,7 @@ class Gravity_Flow_Installation_Wizard {
 	 */
 	public function get_step_index( $step ) {
 		$i = array_search( $step->get_name(), array_keys( $this->_step_class_names ) );
+
 		return $i;
 	}
 
@@ -332,4 +348,5 @@ class Gravity_Flow_Installation_Wizard {
 			$step->flush_values();
 		}
 	}
+
 }
