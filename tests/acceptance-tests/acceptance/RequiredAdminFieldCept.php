3f0fd37bd219aea6b07e569de6c162425ade4cd6
Steve Henty <stevehenty@gmail.com>
2016-08-18 15:30:04 +0200

    autoload forms in acceptance tests

---
+++ tests/acceptance-tests/acceptance/RequiredAdminFieldCept.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+$I = new AcceptanceTester( $scenario );
+
+$I->wantTo( 'Test the field conditional logic on the user input step' );
+
+
+// Submit the form
+$I->amOnPage( '/user-input' );
+
+$I->see( 'User Input' );
+$I->scrollTo( [ 'css' => '.gform_title' ], 20, 50 ); // needed for chromedriver
+$I->fillField( 'Paragraph', 'Some text' );
+$I->scrollTo( [ 'css' => 'input[type=submit]' ], 20, 50 ); // needed for chromedriver
+// Next page
+$I->click( 'Submit' );
+$I->see( 'Thanks for contacting us!' );
+
+// Login to wp-admin
+$I->loginAsAdmin();
+$I->amOnPage( '/wp-admin' );
+
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+//$I->makeScreenshot('inbox');
+$I->see( 'Workflow Inbox' );
+$I->click( 'User Input' );
+
+$I->selectOption( 'input[name=gravityflow_status]', 'complete' );
+$I->click( 'Update' );
+// Approve
+$I->seeElement( 'div.validation_message' );
+
+// Check field conditional logic on the user input step
+$I->see( 'This field is required' );
+
+$I->selectOption( 'input[name=input_13]', 'Second Choice' );
+
+$I->selectOption( 'input[name=gravityflow_status]', 'complete' );
+$I->click( 'Update' );
+$I->seeElement( 'div.notice-success');
+$I->see( 'Entry updated and marked complete' );
--- tests/acceptance-tests/acceptance/_bootstrap.php
+++ tests/acceptance-tests/acceptance/_bootstrap.php
@@ -1,29 +1,36 @@
 <?php
 
-global $wp_rewrite;
+global $wp_rewrite, $wpdb;
 $wp_rewrite->set_permalink_structure( '/%postname%/' );
 $wp_rewrite->flush_rules();
 
 
 GFFormsModel::drop_tables();
+
+// Flush out the feeds
+$wpdb->query( "TRUNCATE TABLE {$wpdb->prefix}gf_addon_feed" );
 GFForms::setup( true );
 update_option( 'gform_pending_installation', false );
-gravity_flow()->setup();
 update_option( 'gravityflow_pending_installation', false );
 
 
 function setup_gravity_forms_pages() {
-	$form_filenames = array( 'conditional-logic.json' );
+	require_once( GFCommon::get_base_path() . '/export.php' );
+
+	$form_filenames = glob( dirname( __FILE__ ) . '/../_data/forms/*.json' );
+
 	foreach ( $form_filenames as $filename ) {
-		$form_json = file_get_contents( dirname( __FILE__ ) . '/../_data/forms/' . $filename );
-		$forms     = json_decode( $form_json, true );
-		$form      = $forms[0];
-		$form_id   = GFAPI::add_form( $form );
-		$slug = str_replace( '.json', '', $filename );
+
+		GFExport::import_file( $filename );
+	}
+
+	$forms = GFAPI::get_forms( false );
+	foreach ( $forms as $form ) {
+		GFFormsModel::update_form_active( $form['id'], true );
 		$page = array(
 			'post_type' => 'page',
-			'post_content' => '[gravityform id=' . $form_id . ']',
-			'post_name' => sanitize_key( $slug ),
+			'post_content' => '[gravityform id=' . $form['id'] . ']',
+			'post_name' => sanitize_title_with_dashes( $form['title'] ),
 			'post_parent' => 0,
 			'post_author' => 1,
 			'post_status' => 'publish',
