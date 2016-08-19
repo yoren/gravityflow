3c61c8169ad982087f3c61fe9b7c9cdd6ade4335
Ovidiu Irodiu <ovidiu@wpriders.com>
2016-08-19 16:02:01 +0300

    Add Two Pages Rejected Test

---
+++ tests/acceptance-tests/acceptance/TwoPagesRejectedCept.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Test summary: two pages rejected
+ *
+ * Test details:
+ * - Select Third Choise radio button
+ * - Go to step 2
+ * - Select Third Choise from select box
+ * - Login to back-end, go to Inbox
+ * - Click on the 'Two pages rejected' Workflow
+ * - Check if Reject button is shown
+ * - Click on Reject button
+ * -
+ */
+
+$I = new AcceptanceTester( $scenario );
+
+$I->wantTo( 'Test the two pages on the user input step' );
+
+// Submit the form
+$I->amOnPage( '/two-pages-rejected' );
+
+$I->see( 'Two pages rejected' );
+$I->scrollTo( [ 'css' => '.gform_title' ], 20, 50 ); // needed for chromedriver
+$I->selectOption( 'input[name=input_7]', 'Third Choice' );
+$I->scrollTo( [ 'css' => '.gform_page_footer .gform_next_button' ], 20, 50 ); // needed for chromedriver
+// Next page
+$I->click( '.gform_page_footer .gform_next_button' );
+$I->selectOption( 'select[name=input_20]', 'Third Choice' );
+$I->click( 'input[type=submit]' );
+$I->see( 'Thanks for contacting us! We will get in touch with you shortly.' );
+
+// Login to wp-admin
+$I->loginAsAdmin();
+$I->amOnPage( '/wp-admin' );
+
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+$I->see( 'Workflow Inbox' );
+$I->click( 'Two pages rejected' );
+
+// Reject
+$I->click( 'Reject' );
+
+// Complete
+$I->see( 'Status: Rejected' );
