26c9444d53e0b5dce8c987e785df9aa9f4c710d0
Ovidiu Irodiu <ovidiu@wpriders.com>
2016-08-23 15:50:35 +0300

    Add Job Apply Agency test

---
+++ tests/acceptance-tests/acceptance/JobApplyAgencyCept.php
@@ -0,0 +1,146 @@
+<?php
+/*
+ * Test summary: Job Apply Agency
+ *
+ * Test details:
+ * - Fill up the fields
+ * - Submit form
+ * - First approval depending on filled up fields (Conditional Routing)
+ * - Modify field number and/or email (Condition for this step)
+ * - Send further for approval
+ * - Revert to verify the field number and/or email
+ * - Update the field number and/or email
+ * - Final approval
+ * - Send notification
+ */
+
+$I = new AcceptanceTester( $scenario );
+
+$I->wantTo( 'Job Apply Agency' );
+
+// Submit the form
+$I->amOnPage( '/job-apply-agency' );
+
+$I->see( 'Job Apply Agency' );
+$I->scrollTo( [ 'css' => '.gform_title' ], 20, 50 ); // needed for chromedriver
+
+$I->fillField( 'input[name="input_3.3"]', 'First' );
+$I->fillField( 'input[name="input_3.6"]', 'Last' );
+$I->selectOption( 'select[name="input_2"]', 'Female' );
+$I->selectOption( 'select[name="input_6"]', '65 or Above' );
+$I->selectOption( 'select[name="input_7"]', 'Homemaker' );
+$I->selectOption( 'select[name="input_8"]', 'Seasonal' );
+$I->scrollTo( [ 'css' => 'input[type=submit]' ], 20, 50 ); // needed for chromedriver
+$I->click( 'Submit' );
+$I->see( 'Thanks for contacting us! We will get in touch with you shortly.' );
+
+// Login as admin2
+$I->loginAs( 'admin2', 'admin2' );
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+$I->see( 'Workflow Inbox' );
+$I->click( 'Job apply agency' );
+
+// Approve
+$I->fillField( 'textarea[name="gravityflow_note"]', 'Added to DB' );
+$I->seeElement( 'button[value=approved]' );
+$I->click( 'button[value=approved]' );
+
+// Check data
+$I->fillField( 'input[name="input_4"]', '0210000000' );
+$I->fillField( 'textarea[name="gravityflow_note"]', 'Phone added' );
+$I->click( 'Update' );
+
+// Log out
+$I->amOnPage( '/specific-assign' );
+$I->click( 'Log out' );
+
+// Login as admin1
+$I->loginAs( 'admin1', 'admin1' );
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+$I->see( 'Workflow Inbox' );
+$I->click( 'Job apply agency' );
+
+// Check data
+$I->fillField( 'input[name="input_5"]', 'example@example.com' );
+$I->fillField( 'textarea[name="gravityflow_note"]', 'Email added' );
+$I->click( 'Update' );
+
+// Log out
+$I->amOnPage( '/specific-assign' );
+$I->click( 'Log out' );
+
+// Login as Admin
+$I->loginAsAdmin();
+$I->amOnPage( '/wp-admin' );
+
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+$I->see( 'Workflow Inbox' );
+$I->click( 'Job apply agency' );
+
+// Revert
+$I->fillField( 'textarea[name="gravityflow_note"]', 'Phone & email recheck' );
+$I->seeElement( 'button[value=revert]' );
+$I->click( 'button[value=revert]' );
+
+// Log out
+$I->amOnPage( '/specific-assign' );
+$I->click( 'Log out' );
+
+// Login as admin2
+$I->loginAs( 'admin2', 'admin2' );
+
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+$I->see( 'Workflow Inbox' );
+$I->click( 'Job apply agency' );
+
+// Check data
+$I->fillField( 'input[name="input_4"]', '0210000000' );
+$I->fillField( 'textarea[name="gravityflow_note"]', 'Phone rechecked' );
+$I->click( 'Update' );
+
+// Log out
+$I->amOnPage( '/specific-assign' );
+$I->click( 'Log out' );
+
+// Login as admin1
+$I->loginAs( 'admin1', 'admin1' );
+
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+$I->see( 'Workflow Inbox' );
+$I->click( 'Job apply agency' );
+
+// Check data
+$I->fillField( 'input[name="input_5"]', 'example@example.com' );
+$I->fillField( 'textarea[name="gravityflow_note"]', 'Email rechecked' );
+$I->click( 'Update' );
+
+// Log out
+$I->amOnPage( '/specific-assign' );
+$I->click( 'Log out' );
+
+// Login as Admin
+$I->loginAsAdmin();
+$I->amOnPage( '/wp-admin' );
+
+// Go to Inbox
+$I->click( 'Workflow' );
+$I->click( 'Inbox' );
+$I->see( 'Workflow Inbox' );
+$I->click( 'Job apply agency' );
+
+// Approve
+$I->fillField( 'textarea[name="gravityflow_note"]', 'Entry approved' );
+$I->seeElement( 'button[value=approved]' );
+$I->click( 'button[value=approved]' );
+
+$I->see( 'Status: Approved' );
