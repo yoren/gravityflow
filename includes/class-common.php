f0c4f1206b35019dd547755a208d630d18669a08
richardW8k <richard@wawrzyniak.me>
2016-10-25 17:37:07 +0100

    Fixed workflow link/url merge tags

--- includes/class-common.php
+++ includes/class-common.php
@@ -32,17 +32,19 @@ class Gravity_Flow_Common {
 			$access_token               = $access_token ? $access_token : gravity_flow()->generate_access_token( $assignee, null, $token_expiration_timestamp );
 		}
 
-		if ( empty( $page_id ) || $page_id == 'admin' ) {
-			$base_url = admin_url( 'admin.php' );
-		} else {
+		$base_url = '';
+		if ( ! empty( $page_id ) && $page_id != 'admin' ) {
 			$base_url = get_permalink( $page_id );
 		}
-		$url = add_query_arg( $query_args, $base_url );
 
-		if ( ! empty( $access_token ) ) {
-			$url = add_query_arg( array( 'gflow_access_token' => $access_token ), $url );
+		if ( empty( $base_url ) ) {
+			$base_url = admin_url( 'admin.php' );
 		}
 
-		return $url;
+		if ( ! empty( $access_token ) ) {
+			$query_args['gflow_access_token'] = $access_token;
+		}
+
+		return add_query_arg( $query_args, $base_url );
 	}
 }
