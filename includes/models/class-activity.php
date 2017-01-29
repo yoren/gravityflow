187c77519da897097d7515cb7516c047e57019c0
Steve Henty <stevehenty@gmail.com>
2017-01-29 16:49:49 +0100

    exclude deleted and trashed entries from reports and activity

--- includes/models/class-activity.php
+++ includes/models/class-activity.php
@@ -13,14 +13,24 @@ class Gravity_Flow_Activity {
 		return $wpdb->prefix . 'gravityflow_activity_log';
 	}
 
+	public static function get_lead_table_name() {
+		return GFFormsModel::get_lead_table_name();
+	}
+
 	public static function get_events( $limit = 400, $objects = array( 'workflow', 'step', 'assignee' ) ) {
 		global $wpdb;
 
 		$log_objects_placeholders = array_fill( 0, count( $objects ), '%s' );
 		$log_objects_in_list = $wpdb->prepare( implode( ', ', $log_objects_placeholders ), $objects );
 
-		$table   = self::get_activity_log_table_name();
-		$sql     = $wpdb->prepare( "SELECT * FROM {$table} WHERE log_object IN ( $log_objects_in_list ) ORDER BY id DESC LIMIT %d", $limit );
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table = self::get_lead_table_name();
+		$sql     = $wpdb->prepare( "
+SELECT * FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
+WHERE a.log_object IN ( $log_objects_in_list )
+
+ORDER BY a.id DESC LIMIT %d", $limit );
 		$results = $wpdb->get_results( $sql );
 
 		return $results;
@@ -29,7 +39,8 @@ class Gravity_Flow_Activity {
 	public static function get_report_data_for_all_forms( $start_date, $end_date = '' ) {
 		global $wpdb;
 
-		$table   = self::get_activity_log_table_name();
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table     = self::get_lead_table_name();
 
 		$form_ids = self::get_form_ids();
 		if ( empty( $form_ids ) ) {
@@ -37,15 +48,16 @@ class Gravity_Flow_Activity {
 		}
 		$in_str_arr    = array_fill( 0, count( $form_ids ), '%d' );
 		$in_str        = join( ',', $in_str_arr );
-		$form_id_clause = $wpdb->prepare( "AND form_id IN ($in_str)", $form_ids );
+		$form_id_clause = $wpdb->prepare( "AND a.form_id IN ($in_str)", $form_ids );
 
 		$sql     = $wpdb->prepare( "
-SELECT form_id, count(id) as c, ROUND( AVG(duration) ) as av
-FROM {$table}
-WHERE log_object = 'workflow' AND log_event = 'ended'
-AND date_created >= %s
+SELECT a.form_id, count(a.id) as c, ROUND( AVG(duration) ) as av
+FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
+WHERE a.log_object = 'workflow' AND a.log_event = 'ended'
+AND a.date_created >= %s
 {$form_id_clause}
-GROUP BY form_id", $start_date );
+GROUP BY a.form_id", $start_date );
 
 		$results = $wpdb->get_results( $sql );
 
@@ -57,15 +69,17 @@ GROUP BY form_id", $start_date );
 		global $wpdb;
 
 
-		$table   = self::get_activity_log_table_name();
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table     = self::get_lead_table_name();
 
 		$sql     = $wpdb->prepare( "
-SELECT MONTH(date_created) as month, count(id) as c, ROUND( AVG(duration) ) as av
-FROM {$table}
+SELECT MONTH(a.date_created) as month, count(a.id) as c, ROUND( AVG(a.duration) ) as av
+FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
 WHERE log_object = 'workflow' AND log_event = 'ended'
-  AND form_id = %d
-  AND date_created >= %s
-GROUP BY YEAR(date_created), MONTH(date_created)", $form_id, $start_date );
+  AND a.form_id = %d
+  AND a.date_created >= %s
+GROUP BY YEAR(a.date_created), MONTH(a.date_created)", $form_id, $start_date );
 
 		$results = $wpdb->get_results( $sql );
 
@@ -76,15 +90,17 @@ GROUP BY YEAR(date_created), MONTH(date_created)", $form_id, $start_date );
 	public static function get_report_data_for_form_by_step( $form_id, $start_date, $end_date = '' ) {
 		global $wpdb;
 
-		$table   = self::get_activity_log_table_name();
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table     = self::get_lead_table_name();
 
 		$sql     = $wpdb->prepare( "
-SELECT feed_id, count(id) as c, ROUND( AVG(duration) ) as av
-FROM {$table}
+SELECT a.feed_id, count(a.id) as c, ROUND( AVG(a.duration) ) as av
+FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
 WHERE log_object = 'step' AND log_event = 'ended'
-  AND form_id = %d
-  AND date_created >= %s
-GROUP BY feed_id", $form_id, $start_date );
+  AND a.form_id = %d
+  AND a.date_created >= %s
+GROUP BY a.feed_id", $form_id, $start_date );
 
 		$results = $wpdb->get_results( $sql );
 
@@ -95,15 +111,17 @@ GROUP BY feed_id", $form_id, $start_date );
 	public static function get_report_data_for_step_by_assignee( $step_id, $start_date, $end_date = '' ) {
 		global $wpdb;
 
-		$table   = self::get_activity_log_table_name();
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table     = self::get_lead_table_name();
 
 		$sql     = $wpdb->prepare( "
-SELECT assignee_id, assignee_type, count(id) as c, ROUND( AVG(duration) ) as av
-FROM {$table}
+SELECT a.assignee_id, a.assignee_type, count(a.id) as c, ROUND( AVG(a.duration) ) as av
+FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
 WHERE log_object = 'assignee' AND log_event = 'status' AND log_value NOT IN ('pending', 'removed')
-  AND feed_id = %d
-  AND date_created >= %s
-GROUP BY assignee_id, assignee_type", $step_id, $start_date );
+  AND a.feed_id = %d
+  AND a.date_created >= %s
+GROUP BY a.assignee_id, a.assignee_type", $step_id, $start_date );
 
 		$results = $wpdb->get_results( $sql );
 
@@ -114,15 +132,17 @@ GROUP BY assignee_id, assignee_type", $step_id, $start_date );
 	public static function get_report_data_for_form_by_assignee( $form_id, $start_date, $end_date = '' ) {
 		global $wpdb;
 
-		$table   = self::get_activity_log_table_name();
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table     = self::get_lead_table_name();
 
 		$sql     = $wpdb->prepare( "
-SELECT assignee_id, assignee_type, count(id) as c, ROUND( AVG(duration) ) as av
-FROM {$table}
-WHERE log_object = 'assignee' AND log_event = 'status' AND log_value NOT IN ('pending', 'removed')
-  AND form_id = %d
-  AND date_created >= %s
-GROUP BY assignee_id, assignee_type", $form_id, $start_date );
+SELECT a.assignee_id, a.assignee_type, count(a.id) as c, ROUND( AVG(a.duration) ) as av
+FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
+WHERE a.log_object = 'assignee' AND a.log_event = 'status' AND a.log_value NOT IN ('pending', 'removed')
+  AND a.form_id = %d
+  AND a.date_created >= %s
+GROUP BY a.assignee_id, a.assignee_type", $form_id, $start_date );
 
 		$results = $wpdb->get_results( $sql );
 
@@ -133,14 +153,16 @@ GROUP BY assignee_id, assignee_type", $form_id, $start_date );
 	public static function get_report_data_for_all_forms_by_assignee( $start_date, $end_date = '' ) {
 		global $wpdb;
 
-		$table   = self::get_activity_log_table_name();
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table     = self::get_lead_table_name();
 
 		$sql     = $wpdb->prepare( "
-SELECT assignee_id, assignee_type, count(id) as c, ROUND( AVG(duration) ) as av
-FROM {$table}
-WHERE log_object = 'assignee' AND log_event = 'status' AND log_value NOT IN ('pending', 'removed')
-  AND date_created >= %s
-GROUP BY assignee_id, assignee_type", $start_date );
+SELECT a.assignee_id, a.assignee_type, count(a.id) as c, ROUND( AVG(a.duration) ) as av
+FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
+WHERE a.log_object = 'assignee' AND a.log_event = 'status' AND log_value NOT IN ('pending', 'removed')
+  AND a.date_created >= %s
+GROUP BY a.assignee_id, a.assignee_type", $start_date );
 
 		$results = $wpdb->get_results( $sql );
 
@@ -151,7 +173,8 @@ GROUP BY assignee_id, assignee_type", $start_date );
 	public static function get_report_data_for_assignee_by_month( $assignee_type, $assignee_id, $start_date, $end_date = '' ) {
 		global $wpdb;
 
-		$table   = self::get_activity_log_table_name();
+		$activity_table = self::get_activity_log_table_name();
+		$lead_table     = self::get_lead_table_name();
 
 		$form_ids = self::get_form_ids();
 		if ( empty( $form_ids ) ) {
@@ -159,16 +182,17 @@ GROUP BY assignee_id, assignee_type", $start_date );
 		}
 		$in_str_arr    = array_fill( 0, count( $form_ids ), '%d' );
 		$in_str        = join( ',', $in_str_arr );
-		$form_id_clause = $wpdb->prepare( "AND form_id IN ($in_str)", $form_ids );
+		$form_id_clause = $wpdb->prepare( "AND a.form_id IN ($in_str)", $form_ids );
 
 		$sql     = $wpdb->prepare( "
-SELECT YEAR(date_created) as year, MONTH(date_created) as month, count(id) as c, ROUND( AVG(duration) ) as av
-FROM {$table}
-WHERE log_object = 'assignee' AND log_event = 'status' AND log_value NOT IN ('pending', 'removed')
-  AND assignee_type = %s AND assignee_id = %s
-  AND date_created >= %s
+SELECT YEAR(a.date_created) as year, MONTH(a.date_created) as month, count(a.id) as c, ROUND( AVG(a.duration) ) as av
+FROM {$activity_table} a
+INNER JOIN {$lead_table} l ON a.lead_id = l.id AND l.status = 'active'
+WHERE a.log_object = 'assignee' AND a.log_event = 'status' AND a.log_value NOT IN ('pending', 'removed')
+  AND a.assignee_type = %s AND a.assignee_id = %s
+  AND a.date_created >= %s
   {$form_id_clause}
-GROUP BY YEAR(date_created), MONTH(date_created)", $assignee_type, $assignee_id, $start_date );
+GROUP BY YEAR(a.date_created), MONTH(a.date_created)", $assignee_type, $assignee_id, $start_date );
 
 		$results = $wpdb->get_results( $sql );
 
