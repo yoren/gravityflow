2fdd260dec09f1c2c2bba8e2aeb7586e07f7d24a
Steve Henty <stevehenty@gmail.com>
2017-02-12 17:35:58 +0100

    do shortcode after merge tags

--- includes/steps/class-step-user-input.php
+++ includes/steps/class-step-user-input.php
@@ -437,9 +437,9 @@ class Gravity_Flow_Step_User_Input extends Gravity_Flow_Step {
 			$note_message = __( 'Entry updated and marked complete.', 'gravityflow' );
 			if ( $this->confirmation_messageEnable ) {
 				$feedback = $this->confirmation_messageValue;
-				$feedback = do_shortcode( $feedback );
 				$feedback = GFCommon::replace_variables( $feedback, $form, $this->get_entry(), false, true, true, 'html' );
 				$feedback = $this->replace_variables( $feedback, $assignee );
+				$feedback = do_shortcode( $feedback );
 				$feedback = wp_kses_post( $feedback );
 			} else {
 				$feedback = $note_message;
