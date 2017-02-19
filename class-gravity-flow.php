fdb6922acd3877660ff6beacb4f506d77233c8c1
Steve Henty <stevehenty@gmail.com>
2017-02-19 21:14:08 +0100

    refactor settings
    Refactored settings_checkbox_and_text() and
    settings_checkbox_and_textarea() to use
    settings_checkbox_and_container()

--- class-gravity-flow.php
+++ class-gravity-flow.php
@@ -1631,83 +1631,27 @@ PRIMARY KEY  (id)
 			<?php
 		}
 
-		public function settings_checkbox_and_text( $field, $echo = true ) {
-			// prepare checkbox
-
-			$checkbox_input = rgars( $field, 'checkbox' );
-
-			$checkbox_field = array(
-				'type'       => 'checkbox',
-				'name'       => $field['name'] . 'Enable',
-				'label'      => esc_html__( 'Enable', 'gravityforms' ),
-				'horizontal' => true,
-				'value'      => '1',
-				'choices'    => false,
-				'tooltip'    => false,
-			);
-
-			$checkbox_field = wp_parse_args( $checkbox_input, $checkbox_field );
-
-			// prepare textbox
-
-			$text_input = rgars( $field, 'text' );
-			$is_enabled   = $this->get_setting( $checkbox_field['name'] );
-
-			$text_field = array(
-				'name'    => $field['name'] . 'Value',
-				'type'    => 'select',
-				'class'   => '',
-				'tooltip' => false,
-			);
-
-			$text_field['class'] .= ' ' . $text_field['name'];
-
-			$text_field = wp_parse_args( $text_input, $text_field );
-
-			// a little more with the checkbox
-			if ( empty( $checkbox_field['choices'] ) ) {
-				$checkbox_field['choices'] = array(
-					array(
-						'name'          => $checkbox_field['name'],
-						'label'         => $checkbox_field['label'],
-						'onchange'      => sprintf( "( function( $, elem ) {
-								$( elem ).parents( 'td' ).css( 'position', 'relative' );
-								if( $( elem ).prop( 'checked' ) ) {
-									$( '%1\$s' ).fadeIn();
-								} else {
-									$( '%1\$s' ).fadeOut();
-								}
-							} )( jQuery, this );",
-						"#{$text_field['name']}Span" ),
-					),
-				);
-			}
-
-			// get markup
-
-			$html = sprintf(
-				'%s <br /><span id="%s" class="%s">%s %s %s</span>',
-				$this->settings_checkbox( $checkbox_field, false ),
-				$text_field['name'] . 'Span',
-				$is_enabled ? '' : 'hidden',
-				esc_html( rgar( $text_field, 'before_input' ) ),
-				$this->settings_text( $text_field, false ),
-				$text_field['tooltip'] ? gform_tooltip( $text_field['tooltip'], rgar( $text_field, 'tooltip_class' ) . ' tooltip ' . $text_field['name'], true ) : ''
-			);
-
-			if ( $echo ) {
-				echo $html;
-			}
-
-			return $html;
-		}
-
+		/**
+		 * Renders or returns markup for a checkbox plus container composite setting.
+		 *
+		 * The container will be displayed or hidden depending on the value of the checkbox field.
+		 *
+		 * @since 1.5.1
+		 *
+		 * @param array $field
+		 * @param bool  $echo
+		 *
+		 * @return string
+		 */
 		public function settings_checkbox_and_container( $field, $echo = true ) {
-			// prepare checkbox
 
-			$checkbox_input = rgars( $field, 'checkbox' );
+			$checkbox_field = rgar( $field, 'checkbox' );
 
-			$checkbox_field = array(
+			if ( empty( $checkbox_field ) ) {
+				return '';
+			}
+
+			$checkbox_defaults = array(
 				'type'       => 'checkbox',
 				'name'       => $field['name'] . 'Enable',
 				'label'      => esc_html__( 'Enable', 'gravityforms' ),
@@ -1717,11 +1661,8 @@ PRIMARY KEY  (id)
 				'tooltip'    => false,
 			);
 
-			$checkbox_field = wp_parse_args( $checkbox_input, $checkbox_field );
+			$checkbox_field = wp_parse_args( $checkbox_field, $checkbox_defaults );
 
-			$is_enabled   = $this->get_setting( $checkbox_field['name'] );
-
-			// a little more with the checkbox
 			if ( empty( $checkbox_field['choices'] ) ) {
 				$checkbox_field['choices'] = array(
 					array(
@@ -1740,12 +1681,16 @@ PRIMARY KEY  (id)
 				);
 			}
 
-			// get markup
+			$field['checkbox'] = $checkbox_field;
+
+			$checkbox_field = rgar( $field, 'checkbox' );
+
+			$is_enabled = $this->get_setting( $checkbox_field['name'] );
 
 			$container_settings_markup = '';
 
 			if ( isset( $field['settings'] ) && is_array( $field['settings'] ) ) {
-				foreach(  $field['settings'] as $setting ) {
+				foreach (  $field['settings'] as $setting ) {
 					if ( ! isset( $setting['type'] ) ) {
 						continue;
 					}
@@ -1754,14 +1699,22 @@ PRIMARY KEY  (id)
 						$container_settings_markup .= rgar( $setting, 'before' );
 						unset( $setting['before'] );
 					}
+					if ( isset( $setting['after'] ) ) {
+						$after = rgar( $setting, 'after' );
+						unset( $setting['after'] );
+					} else {
+						$after = '';
+					}
 					if ( method_exists( $this, $method ) ) {
 						$container_settings_markup .= $this->{$method}( $setting, false );
 					}
+					$container_settings_markup .= isset( $setting['tooltip'] ) ? gform_tooltip( $setting['tooltip'], rgar( $setting, 'tooltip_class' ) . ' tooltip ' . $setting['name'], true ) : '';
+					$container_settings_markup .= $after;
 				}
 			}
 
 			$html = sprintf(
-				'%s <br /><div id="%s" class="%s">%s</div>',
+				'%s <div id="%s" class="%s">%s</div>',
 				$this->settings_checkbox( $checkbox_field, false ),
 				$field['name'] . 'Container',
 				$is_enabled ? '' : 'hidden',
@@ -1775,31 +1728,61 @@ PRIMARY KEY  (id)
 			return $html;
 		}
 
-		public function settings_checkbox_and_textarea( $field, $echo = true ) {
-			// prepare checkbox
+		/**
+		 * Renders or returns a composite setting with a checkbox and text field.
+		 *
+		 * The text field will be hidden or displayed depending on the value of the checkbox.
+		 *
+		 * @since 1.5.1 Updated to use Gravity_Flow::settings_checkbox_and_container()
+		 * @since unknown
+		 *
+		 * @param array $field
+		 * @param bool  $echo
+		 *
+		 * @return string
+		 */
+		public function settings_checkbox_and_text( $field, $echo = true ) {
 
-			$checkbox_input = rgars( $field, 'checkbox' );
-
-			$checkbox_field = array(
-				'type'       => 'checkbox',
-				'name'       => $field['name'] . 'Enable',
-				'label'      => esc_html__( 'Enable', 'gravityforms' ),
-				'horizontal' => true,
-				'value'      => '1',
-				'choices'    => false,
-				'tooltip'    => false,
-			);
-
-			$checkbox_field = wp_parse_args( $checkbox_input, $checkbox_field );
-
-			// prepare textarea
-
-			$textarea_input = rgars( $field, 'textarea' );
-			$is_enabled   = $this->get_setting( $checkbox_field['name'] );
+			$text_input = rgars( $field, 'text' );
 
 			$text_field = array(
 				'name'    => $field['name'] . 'Value',
-				'type'    => 'select',
+				'type'    => 'text',
+				'class'   => '',
+				'tooltip' => false,
+			);
+
+			$text_field['class'] .= ' ' . $text_field['name'];
+
+			$text_field = wp_parse_args( $text_input, $text_field );
+
+			unset( $field['textarea'] );
+
+			$field['settings'] = array( $text_field );
+
+			return $this->settings_checkbox_and_container( $field, $echo );
+		}
+
+		/**
+		 * Renders or returns a composite setting with a checkbox and text field.
+		 *
+		 * The text field will be hidden or displayed depending on the value of the checkbox.
+		 *
+		 * @since 1.5.1 Updated to use Gravity_Flow::settings_checkbox_and_container()
+		 * @since unknown
+		 *
+		 * @param array $field
+		 * @param bool  $echo
+		 *
+		 * @return string
+		 */
+		public function settings_checkbox_and_textarea( $field, $echo = true ) {
+
+			$textarea_input = rgars( $field, 'textarea' );
+
+			$text_field = array(
+				'name'    => $field['name'] . 'Value',
+				'type'    => 'textarea',
 				'class'   => '',
 				'tooltip' => false,
 			);
@@ -1808,42 +1791,11 @@ PRIMARY KEY  (id)
 
 			$text_field = wp_parse_args( $textarea_input, $text_field );
 
-			// a little more with the checkbox
-			if ( empty( $checkbox_field['choices'] ) ) {
-				$checkbox_field['choices'] = array(
-					array(
-						'name'          => $checkbox_field['name'],
-						'label'         => $checkbox_field['label'],
-						'onchange'      => sprintf( "( function( $, elem ) {
-								$( elem ).parents( 'td' ).css( 'position', 'relative' );
-								if( $( elem ).prop( 'checked' ) ) {
-									$( '%1\$s' ).fadeIn();
-								} else {
-									$( '%1\$s' ).fadeOut();
-								}
-							} )( jQuery, this );",
-							"#{$text_field['name']}Span" ),
-					),
-				);
-			}
+			unset( $field['textarea'] );
 
-			// get markup
+			$field['settings'] = array( $text_field );
 
-			$html = sprintf(
-				'%s <br /><span id="%s" class="%s">%s %s %s</span>',
-				$this->settings_checkbox( $checkbox_field, false ),
-				$text_field['name'] . 'Span',
-				$is_enabled ? '' : 'hidden',
-				esc_html( rgar( $text_field, 'before_input' ) ),
-				$this->settings_textarea( $text_field, false ),
-				$text_field['tooltip'] ? gform_tooltip( $text_field['tooltip'], rgar( $text_field, 'tooltip_class' ) . ' tooltip ' . $text_field['name'], true ) : ''
-			);
-
-			if ( $echo ) {
-				echo $html;
-			}
-
-			return $html;
+			return $this->settings_checkbox_and_container( $field, $echo );
 		}
 
 		public function settings_visual_editor( $field ) {
