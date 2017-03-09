ab543aa68f7e6b083cd11309421a5a08c79466fd
Steve Henty <stevehenty@gmail.com>
2017-03-09 15:33:05 +0100

    fix an issue causing an error in GravityView

--- gravityflow.php
+++ gravityflow.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Flow
 Plugin URI: https://gravityflow.io
 Description: Build Workflow Applications with Gravity Forms.
-Version: 1.6.1-dev-1
+Version: 1.6.1-dev-2
 Author: Steven Henty
 Author URI: http://www.stevenhenty.com
 License: GPL-3.0+
@@ -27,7 +27,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see http://www.gnu.org/licenses.
 */
 
-define( 'GRAVITY_FLOW_VERSION', '1.6.1-dev-1' );
+define( 'GRAVITY_FLOW_VERSION', '1.6.1-dev-2' );
 
 define( 'GRAVITY_FLOW_EDD_STORE_URL', 'https://gravityflow.io' );
 
