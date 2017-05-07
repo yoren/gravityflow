5afa30e4dfa837ec4ecfaed29bb637ce19acbf5e
Steve Henty <stevehenty@gmail.com>
2017-05-07 20:33:57 +0200

    version bump

--- gravityflow.php
+++ gravityflow.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Flow
 Plugin URI: https://gravityflow.io
 Description: Build Workflow Applications with Gravity Forms.
-Version: 1.6.2-dev-1
+Version: 1.7.1-dev
 Author: Steven Henty
 Author URI: http://www.stevenhenty.com
 License: GPL-3.0+
