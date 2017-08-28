ab3150f9cddb9a92ba062e5e61b34d88b37fb4d1
richardW8k <richard@wawrzyniak.me>
2017-08-28 14:58:24 +0100

    Support step_id='step name here'

--- includes/merge-tags/class-merge-tag-workflow-note.php
+++ includes/merge-tags/class-merge-tag-workflow-note.php
@@ -83,15 +83,16 @@ class Gravity_Flow_Merge_Tag_Workflow_Note extends Gravity_Flow_Merge_Tag {
 	 *
 	 * @since 1.7.1-dev
 	 *
-	 * @param int      $entry_id The current entry ID.
-	 * @param int|null $step_id  The step ID or null to return the most recent note.
-	 * @param bool     $history  Include notes from previous occurrences of the specified step.
+	 * @param int         $entry_id The current entry ID.
+	 * @param null|string $step_id  The step ID, name, or null to return the most recent note.
+	 * @param bool        $history  Include notes from previous occurrences of the specified step.
 	 *
 	 * @return array
 	 */
 	protected function get_step_notes( $entry_id, $step_id, $history ) {
 		$notes      = Gravity_Flow_Common::get_workflow_notes( $entry_id, true );
 		$step_notes = array();
+		$step_id    = $this->get_step_id_from_name( $step_id );
 
 		$step_found            = false;
 		$step_timestamp        = $step_id && ! $history ? gform_get_meta( $entry_id, 'workflow_step_' . $step_id . '_timestamp' ) : 0;
@@ -117,6 +118,31 @@ class Gravity_Flow_Merge_Tag_Workflow_Note extends Gravity_Flow_Merge_Tag {
 		}
 
 		return $step_notes;
+	}
+
+	/**
+	 * Retrieve the step id for the specified step name.
+	 *
+	 * @since 1.8.1-dev
+	 *
+	 * @param null|string $step_id The step ID, name, or null to return the most recent note.
+	 *
+	 * @return null|string
+	 */
+	protected function get_step_id_from_name( $step_id ) {
+		if ( is_string( $step_id ) && ! is_numeric( $step_id ) ) {
+			$step_name = strtolower( $step_id );
+			$steps     = gravity_flow()->get_steps( $this->form['id'] );
+
+			foreach ( $steps as $step ) {
+				if ( strtolower( $step->get_name() ) === $step_name ) {
+					$step_id = $step->get_id();
+					break;
+				}
+			}
+		}
+
+		return $step_id;
 	}
 
 	/**
