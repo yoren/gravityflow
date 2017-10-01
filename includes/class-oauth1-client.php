8e6c0ea8205e58fd4ae79bb6c44d9f0e54f877e6
Steve Henty <stevehenty@gmail.com>
2017-10-01 15:57:59 -0400

    Rename class

--- includes/class-oauth1-client.php
+++ includes/class-oauth1-client.php
@@ -13,14 +13,13 @@ if ( ! class_exists( 'GFForms' ) ) {
 }
 
 /**
- * A minimal oauth1 client that will authenticate with the oauth server for the WP rest api
- * facilitated by this plugin
+ * A minimal oauth1 client. Currently only supports the official WordPress oauth server plugin for the WP REST API.
  *
- * Class Gravity_Flow_API
+ * Class Gravity_Flow_Oauth1_Client
  *
- * @since 1.0
+ * @since 1.8.1
  */
-class GravityFlow_Oauth1_Client {
+class Gravity_Flow_Oauth1_Client {
 
 	/**
 	 * Gives unique name to the application for storage.
@@ -32,9 +31,9 @@ class GravityFlow_Oauth1_Client {
 	/**
 	 * Class constructor
 	 *
-	 * @param array  $config sent in to build the client.
+	 * @param array  $config                sent in to build the client.
 	 * @param string $connection_identifier a unique identifier for the instance.
-	 * @param string $url the app url used for collecting the auth urls.
+	 * @param string $url                   the app url used for collecting the auth urls.
 	 *
 	 * @throws InvalidArgumentException If missing keys in config.
 	 * @return void
@@ -47,20 +46,20 @@ class GravityFlow_Oauth1_Client {
 		);
 		foreach ( $required_params as $param ) {
 			if ( ! isset( $config[ $param ] ) ) {
-				throw new InvalidArgumentException( sprintf( 'Missing OAuth1 client configuration: %s - see includes/class-gravityflow-oauth1-client.php', $param ) );
+				throw new InvalidArgumentException( sprintf( 'Missing OAuth1 client configuration: %s - see includes/class-oauth1-client.php', $param ) );
 			}
 		}
-		$this->url = $url;
-		$this->config = $config;
+		$this->url       = $url;
+		$this->config    = $config;
 		$this->timestamp = time();
 
 		$this->connection_identifier = $connection_identifier;
-		$this->data_store = array(
-			'auth_urls' => 'gravityflow_oauth_urls_' . base64_encode( $this->connection_identifier ),
-			'progress' => 'gravity_flow_oauth_progress_' . base64_encode( $this->connection_identifier ),
+		$this->data_store            = array(
+			'auth_urls'        => 'gravityflow_oauth_urls_' . base64_encode( $this->connection_identifier ),
+			'progress'         => 'gravity_flow_oauth_progress_' . base64_encode( $this->connection_identifier ),
 			'full_credentials' => 'gravity_flow_oauth_full_credentials_' . base64_encode( $this->connection_identifier ),
 		);
-		$this->api_auth_urls = $this->get_auth_urls();
+		$this->api_auth_urls         = $this->get_auth_urls();
 	}
 
 	/**
@@ -73,18 +72,19 @@ class GravityFlow_Oauth1_Client {
 		if ( get_option( $this->data_store['auth_urls'] ) !== false ) {
 			return get_option( $this->data_store['auth_urls'] );
 		}
-		$url = wp_parse_url( $this->url, PHP_URL_SCHEME ) . '://' . wp_parse_url( $this->url, PHP_URL_HOST );
-		$page = vip_safe_wp_remote_get( $url );
+		$url  = wp_parse_url( $this->url, PHP_URL_SCHEME ) . '://' . wp_parse_url( $this->url, PHP_URL_HOST );
+		$page = wp_remote_get( $url );
 
 		if ( ! is_wp_error( $page ) ) {
-			$headers = $page['headers'];
-			$link_parts = explode( '; ',$headers['link'] );
+			$headers            = $page['headers'];
+			$link_parts         = explode( '; ', $headers['link'] );
 			$this->api_base_url = str_replace( array( '<', '>' ), '', $link_parts[0] );
-			$api_details = vip_safe_wp_remote_get( $this->api_base_url );
+			$api_details        = wp_remote_get( $this->api_base_url );
 			if ( ! is_wp_error( $api_details ) ) {
 				$api_details = json_decode( $api_details['body'], true );
 				if ( isset( $api_details['authentication'] ) ) {
 					update_option( $this->data_store['auth_urls'], $api_details['authentication'] );
+
 					return $api_details['authentication'];
 				} else {
 					throw new Exception( sprintf( 'No authentication array in api details from %s', $this->api_base_url ) );
@@ -104,13 +104,14 @@ class GravityFlow_Oauth1_Client {
 	 * @throws Exception If request for temporary credentials fails
 	 *
 	 * @return array
-	 **/
+	 */
 	function request_token() {
 		$response = wp_remote_post( $this->api_auth_urls['oauth1']['request'], array(
 			'headers' => $this->request_token_headers(),
 		) );
 		if ( ! is_wp_error( $response ) && 200 === (int) $response['response']['code'] ) {
 			parse_str( $response['body'], $temporary_credentials );
+
 			return $temporary_credentials;
 		} else {
 			gravity_flow()->log_debug( __METHOD__ . '() - response: ' . print_r( $response, true ) );
@@ -121,9 +122,10 @@ class GravityFlow_Oauth1_Client {
 	/**
 	 * Gets request header for final access request
 	 *
-	 * @param string $url Url for the request.
+	 * @param string $url       Url for the request.
 	 * @param string $http_verb Request type (GET, POST etc).
-	 * @param array  $options Optional array of options.
+	 * @param array  $options   Optional array of options.
+	 *
 	 * @return string
 	 **/
 	function get_full_request_header( $url, $http_verb, $options = array() ) {
@@ -142,6 +144,7 @@ class GravityFlow_Oauth1_Client {
 	 * Request access token from oauth server
 	 *
 	 * @param string $verifier The code sent back after authorizing at remote site.
+	 *
 	 * @throws Exception If remote request fails.
 	 *
 	 * @return array
@@ -152,6 +155,7 @@ class GravityFlow_Oauth1_Client {
 		) );
 		if ( ! is_wp_error( $response ) && 200 === (int) $response['response']['code'] ) {
 			parse_str( $response['body'], $access_credentials );
+
 			return $access_credentials;
 		} else {
 			gravity_flow()->log_debug( __METHOD__ . '() - response: ' . print_r( $response, true ) );
@@ -163,7 +167,7 @@ class GravityFlow_Oauth1_Client {
 	 * Get headers for token request
 	 *
 	 * @return array
-	 **/
+	 */
 	function request_token_headers() {
 		$parameters = $this->request_token_params();
 
@@ -178,8 +182,9 @@ class GravityFlow_Oauth1_Client {
 	 * Get headers for access token request
 	 *
 	 * @param string $verifier Code sent back after authorizing app on remote site.
+	 *
 	 * @return array
-	 **/
+	 */
 	function request_access_token_headers( $verifier ) {
 		$parameters = $this->request_access_token_params( $verifier );
 
@@ -194,7 +199,7 @@ class GravityFlow_Oauth1_Client {
 	 * Generates nonce for oauth requests
 	 *
 	 * @return string
-	 **/
+	 */
 	public function nonce() {
 		return md5( mt_rand() );
 	}
@@ -203,14 +208,14 @@ class GravityFlow_Oauth1_Client {
 	 * Sets up params for request token request
 	 *
 	 * @return array
-	 **/
+	 */
 	function request_token_params() {
 		return array(
-			'oauth_consumer_key' => $this->config['consumer_key'],
-			'oauth_nonce' => $this->nonce(),
+			'oauth_consumer_key'     => $this->config['consumer_key'],
+			'oauth_nonce'            => $this->nonce(),
 			'oauth_signature_method' => 'HMAC-SHA1',
-			'oauth_timestamp' => $this->timestamp,
-			'oauth_callback' => $this->config['callback_url'],
+			'oauth_timestamp'        => $this->timestamp,
+			'oauth_callback'         => $this->config['callback_url'],
 		);
 	}
 
@@ -218,14 +223,14 @@ class GravityFlow_Oauth1_Client {
 	 * Sets up params for full request
 	 *
 	 * @return array
-	 **/
+	 */
 	function full_request_params() {
 		return array(
-			'oauth_consumer_key' => $this->config['consumer_key'],
-			'oauth_token' => $this->config['token'],
-			'oauth_nonce' => $this->nonce(),
+			'oauth_consumer_key'     => $this->config['consumer_key'],
+			'oauth_token'            => $this->config['token'],
+			'oauth_nonce'            => $this->nonce(),
 			'oauth_signature_method' => 'HMAC-SHA1',
-			'oauth_timestamp' => $this->timestamp,
+			'oauth_timestamp'        => $this->timestamp,
 		);
 	}
 
@@ -233,27 +238,29 @@ class GravityFlow_Oauth1_Client {
 	 * Sets up params for request access token request
 	 *
 	 * @param string $verifier Verification code sent back after authorizing app at remote site.
+	 *
 	 * @return array
-	 **/
+	 */
 	function request_access_token_params( $verifier ) {
 		return array(
-			'oauth_consumer_key' => $this->config['consumer_key'],
-			'oauth_nonce' => $this->nonce(),
+			'oauth_consumer_key'     => $this->config['consumer_key'],
+			'oauth_nonce'            => $this->nonce(),
 			'oauth_signature_method' => 'HMAC-SHA1',
-			'oauth_timestamp' => $this->timestamp,
-			'oauth_verifier' => $verifier,
-			'oauth_token' => $this->config['token'],
+			'oauth_timestamp'        => $this->timestamp,
+			'oauth_verifier'         => $verifier,
+			'oauth_token'            => $this->config['token'],
 		);
 	}
 
 	/**
 	 * Signs oauth request
 	 *
-	 * @param string $uri Url used to sign the request.
+	 * @param string $uri        Url used to sign the request.
 	 * @param array  $parameters Parameters to build into signature.
-	 * @param string $http_verb Request type.
+	 * @param string $http_verb  Request type.
+	 *
 	 * @return string
-	 **/
+	 */
 	function hmac_sign( $uri, array $parameters = array(), $http_verb = 'POST' ) {
 		$base_string = $this->base_string( $uri, $parameters, $http_verb );
 
@@ -263,11 +270,12 @@ class GravityFlow_Oauth1_Client {
 	/**
 	 * Builds base string for request signing
 	 *
-	 * @param string $uri Url in the request.
+	 * @param string $uri        Url in the request.
 	 * @param array  $parameters Params from the request.
-	 * @param string $http_verb Request method.
+	 * @param string $http_verb  Request method.
+	 *
 	 * @return string
-	 **/
+	 */
 	public function base_string( $uri, array $parameters = array(), $http_verb = 'POST' ) {
 		ksort( $parameters );
 
@@ -280,7 +288,7 @@ class GravityFlow_Oauth1_Client {
 	 * Get key:secret pair for request
 	 *
 	 * @return string
-	 **/
+	 */
 	public function key() {
 		$key = rawurlencode( $this->config['consumer_secret'] ) . '&';
 
@@ -295,8 +303,9 @@ class GravityFlow_Oauth1_Client {
 	 * Hash request
 	 *
 	 * @param array $data Data to be included in the hash.
+	 *
 	 * @return array
-	 **/
+	 */
 	public function hash( $data ) {
 		return hash_hmac( 'sha1', $data, $this->key(), true );
 	}
@@ -305,13 +314,12 @@ class GravityFlow_Oauth1_Client {
 	 * Build header for request
 	 *
 	 * @param array $parameters Oauth parameters to be sent.
+	 *
 	 * @return array
-	 **/
+	 */
 	public function authorization_headers( array $parameters ) {
 		$parameters = http_build_query( $parameters, '', ', ', PHP_QUERY_RFC3986 );
 
 		return "OAuth $parameters";
 	}
-
-
 }
