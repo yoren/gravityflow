0fc4e3d7593e733bbe15eee9f7d962e029e32946
Richard Wawrzyniak <richard@wawrzyniak.me>
2017-12-10 20:06:03 +0000

    Fixed Gravity_Flow_Field_Assignee_Select docs issues

--- includes/fields/class-field-assignee-select.php
+++ includes/fields/class-field-assignee-select.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Gravity Flow Assignee Field
+ *
+ * @package     GravityFlow
+ * @copyright   Copyright (c) 2015-2017, Steven Henty S.L.
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
 
 if ( ! class_exists( 'GFForms' ) ) {
 	die();
@@ -6,24 +13,43 @@ if ( ! class_exists( 'GFForms' ) ) {
 
 /**
  * Class Gravity_Flow_Field_Assignee_Select
- *
- * @copyright   Copyright (c) 2015-2017, Steven Henty S.L.
- * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
 class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 
+	/**
+	 * The field type.
+	 *
+	 * @var string
+	 */
 	public $type = 'workflow_assignee_select';
 
+	/**
+	 * Indicates if this field type can be used when configuring conditional logic rules.
+	 *
+	 * @return bool
+	 */
 	public function is_conditional_logic_supported() {
 		return false;
 	}
 
+	/**
+	 * Adds the Workflow Fields group to the form editor.
+	 *
+	 * @param array $field_groups The properties for the field groups.
+	 *
+	 * @return array
+	 */
 	public function add_button( $field_groups ) {
 		$field_groups = Gravity_Flow_Fields::maybe_add_workflow_field_group( $field_groups );
 
 		return parent::add_button( $field_groups );
 	}
 
+	/**
+	 * Returns the class names of the settings which should be available on the field in the form editor.
+	 *
+	 * @return array
+	 */
 	function get_form_editor_field_settings() {
 		return array(
 			'conditional_logic_field_setting',
@@ -45,6 +71,11 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 		);
 	}
 
+	/**
+	 * Returns the field button properties for the form editor.
+	 *
+	 * @return array
+	 */
 	public function get_form_editor_button() {
 		return array(
 			'group' => 'workflow_fields',
@@ -52,10 +83,22 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 		);
 	}
 
+	/**
+	 * Returns the field title.
+	 *
+	 * @return string
+	 */
 	public function get_form_editor_field_title() {
 		return __( 'Assignee', 'gravityflow' );
 	}
 
+	/**
+	 * Return the HTML markup for the field choices.
+	 *
+	 * @param string $value The field value.
+	 *
+	 * @return string
+	 */
 	public function get_choices( $value ) {
 
 		$include_users  = (bool) $this->gravityflowAssigneeFieldShowUsers;
@@ -67,6 +110,16 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 		return $choices;
 	}
 
+	/**
+	 * Return the HTML markup for the field choices.
+	 *
+	 * @param string $value          The field value.
+	 * @param bool   $include_users  Indicates if the users should be added as choices.
+	 * @param bool   $include_roles  Indicates if the roles should be added as choices.
+	 * @param bool   $include_fields Indicates if the fields should be added as choices.
+	 *
+	 * @return string
+	 */
 	public function get_assignees_as_choices( $value, $include_users = true, $include_roles = true, $include_fields = true ) {
 		$form_id         = $this->formId;
 		$account_choices = $role_choices = $fields_choices = $optgroups = array();
@@ -154,6 +207,17 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 		return $html;
 	}
 
+	/**
+	 * Return the entry value for display on the entries list page.
+	 *
+	 * @param string|array $value    The field value.
+	 * @param array        $entry    The Entry Object currently being processed.
+	 * @param string       $field_id The field or input ID currently being processed.
+	 * @param array        $columns  The properties for the columns being displayed on the entry list page.
+	 * @param array        $form     The Form Object currently being processed.
+	 *
+	 * @return string
+	 */
 	public function get_value_entry_list( $value, $entry, $field_id, $columns, $form ) {
 		$assignee = parent::get_value_entry_list( $value, $entry, $field_id, $columns, $form );
 		$value    = $this->get_display_name( $assignee );
@@ -161,13 +225,39 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 		return $value;
 	}
 
-
+	/**
+	 * Return the entry value which will replace the field merge tag.
+	 *
+	 * @param string       $value      The field value. Depending on the location the merge tag is being used the following functions may have already been applied to the value: esc_html, nl2br, and urlencode.
+	 * @param string       $input_id   The field or input ID from the merge tag currently being processed.
+	 * @param array        $entry      The Entry Object currently being processed.
+	 * @param array        $form       The Form Object currently being processed.
+	 * @param string       $modifier   The merge tag modifier. e.g. value.
+	 * @param string|array $raw_value  The raw field value from before any formatting was applied to $value.
+	 * @param bool         $url_encode Indicates if the urlencode function may have been applied to the $value.
+	 * @param bool         $esc_html   Indicates if the esc_html function may have been applied to the $value.
+	 * @param string       $format     The format requested for the location the merge is being used. Possible values: html, text or url.
+	 * @param bool         $nl2br      Indicates if the nl2br function may have been applied to the $value.
+	 *
+	 * @return string
+	 */
 	public function get_value_merge_tag( $value, $input_id, $entry, $form, $modifier, $raw_value, $url_encode, $esc_html, $format, $nl2br ) {
 		$value = $this->get_display_name( $value, $modifier, $url_encode, $esc_html );
 
 		return $value;
 	}
 
+	/**
+	 * Return the entry value for display on the entry detail page and for the {all_fields} merge tag.
+	 *
+	 * @param string     $value    The field value.
+	 * @param string     $currency The entry currency code.
+	 * @param bool|false $use_text When processing choice based fields should the choice text be returned instead of the value.
+	 * @param string     $format   The format requested for the location the merge is being used. Possible values: html, text or url.
+	 * @param string     $media    The location where the value will be displayed. Possible values: screen or email.
+	 *
+	 * @return string
+	 */
 	public function get_value_entry_detail( $value, $currency = '', $use_text = false, $format = 'html', $media = 'screen' ) {
 		$assignee = parent::get_value_entry_detail( $value, $currency, $use_text, $format, $media );
 		$value    = $this->get_display_name( $assignee );
@@ -175,6 +265,16 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 		return $value;
 	}
 
+	/**
+	 * Gets the display name for the selected choice (assignee).
+	 *
+	 * @param string $assignee   The assignee key.
+	 * @param string $modifier   The merge tag modifier.
+	 * @param bool   $url_encode Indicates if the urlencode function may have been applied to the $value.
+	 * @param bool   $esc_html   Indicates if the esc_html function may have been applied to the $value.
+	 *
+	 * @return string
+	 */
 	public function get_display_name( $assignee, $modifier = '', $url_encode = false, $esc_html = true ) {
 		if ( empty( $assignee ) ) {
 			return '';
@@ -194,10 +294,10 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 	/**
 	 * Format the entry value before it is used in entry exports and by framework add-ons using GFAddOn::get_field_value().
 	 *
-	 * @param array $entry The entry currently being processed.
-	 * @param string $input_id The field or input ID.
+	 * @param array      $entry    The entry currently being processed.
+	 * @param string     $input_id The field or input ID.
 	 * @param bool|false $use_text When processing choice based fields should the choice text be returned instead of the value.
-	 * @param bool|false $is_csv Is the value going to be used in the .csv entries export?
+	 * @param bool|false $is_csv   Indicates if the value is going to be used in the .csv entries export.
 	 *
 	 * @return string
 	 */
@@ -209,6 +309,9 @@ class Gravity_Flow_Field_Assignee_Select extends GF_Field_Select {
 		return $this->get_display_name( rgar( $entry, $input_id ) );
 	}
 
+	/**
+	 * Sanitize the field settings when the form is saved.
+	 */
 	public function sanitize_settings() {
 		parent::sanitize_settings();
 		if ( ! empty( $this->gravityflowUsersRoleFilter ) ) {
