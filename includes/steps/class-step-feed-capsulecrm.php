afd062c39c15f8d16320b91ef54a8be4308e04c2
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:18:56 +0000

    Fixed Gravity_Flow_Step_Feed_CapsuleCRM docs

--- includes/steps/class-step-feed-capsulecrm.php
+++ includes/steps/class-step-feed-capsulecrm.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Capsule CRM
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_CapsuleCRM
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,15 +13,41 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_CapsuleCRM
+ */
 class Gravity_Flow_Step_Feed_CapsuleCRM extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'capsulecrm';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFCapsuleCRM';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Capsule CRM';
 	}
 
+	/**
+	 * Returns the feed name.
+	 *
+	 * @param array $feed The Capsule CRM feed properties.
+	 *
+	 * @return string
+	 */
 	public function get_feed_label( $feed ) {
 		$label = $feed['meta']['feed_name'];
 
