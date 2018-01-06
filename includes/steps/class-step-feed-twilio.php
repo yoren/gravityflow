a79a91c1c2492952e040d7dd8930a41ba73b2eba
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 16:09:45 +0000

    Fixed Gravity_Flow_Step_Feed_Twilio docs

--- includes/steps/class-step-feed-twilio.php
+++ includes/steps/class-step-feed-twilio.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Twlio
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_Twilio
  * @copyright   Copyright (c) 2015-2018, Steven Henty S.L.
@@ -14,15 +13,39 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_Twilio
+ */
 class Gravity_Flow_Step_Feed_Twilio extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'twilio';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFTwilio';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Twilio';
 	}
 
+	/**
+	 * Returns the URL for the step icon.
+	 *
+	 * @return string
+	 */
 	public function get_icon_url() {
 		return $this->get_base_url() . '/images/twilio-icon-red.svg';
 	}
