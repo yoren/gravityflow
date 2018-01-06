00ccf281253094f20e6bb58f56dd37e2d0b02bb2
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 14:51:15 +0000

    Fixed Gravity_Flow_Step_Feed_Campaign_Monitor docs

--- includes/steps/class-step-feed-campaign-monitor.php
+++ includes/steps/class-step-feed-campaign-monitor.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Campaign Monitor
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_Campaign_Monitor
  * @copyright   Copyright (c) 2015-2018, Steven Henty S.L.
@@ -14,11 +13,30 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_Campaign_Monitor
+ */
 class Gravity_Flow_Step_Feed_Campaign_Monitor extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'campaign_monitor';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFCampaignMonitor';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Campaign Monitor';
 	}
