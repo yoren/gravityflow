400be444775d6d2a5327b5428ffb82108514ff94
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 14:48:03 +0000

    Fixed Gravity_Flow_Step_Feed_Batchbook docs

--- includes/steps/class-step-feed-batchbook.php
+++ includes/steps/class-step-feed-batchbook.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Batchbook
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_Batchbook
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,15 +13,41 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_Batchbook
+ */
 class Gravity_Flow_Step_Feed_Batchbook extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'batchbook';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFBatchbook';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Batchbook';
 	}
 
+	/**
+	 * Returns the feed name.
+	 *
+	 * @param array $feed The Batchbook feed properties.
+	 *
+	 * @return string
+	 */
 	public function get_feed_label( $feed ) {
 		$label = $feed['meta']['feed_name'];
 
