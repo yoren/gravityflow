d9bd8b963a9ba0a57cbd3faaca20c40abb5eff5b
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 14:45:18 +0000

    Fixed Gravity_Flow_Step_Feed_AgileCRM docs

--- includes/steps/class-step-feed-agilecrm.php
+++ includes/steps/class-step-feed-agilecrm.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Agile CRM
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_AgileCRM
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,15 +13,39 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_AgileCRM
+ */
 class Gravity_Flow_Step_Feed_AgileCRM extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'agilecrm';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFAgileCRM';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Agile CRM';
 	}
 
+	/**
+	 * Returns the URL for the step icon.
+	 *
+	 * @return string
+	 */
 	public function get_icon_url() {
 		return $this->get_base_url() . '/images/agilecrm-icon.svg';
 	}
