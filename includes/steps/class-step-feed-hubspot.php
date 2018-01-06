9d4019c92596ca7cee18838496325074b2ac1a4e
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:45:14 +0000

    Fixed Gravity_Flow_Step_Feed_HubSpot docs

--- includes/steps/class-step-feed-hubspot.php
+++ includes/steps/class-step-feed-hubspot.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed HubSpot
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_HubSpot
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,16 +13,46 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_HubSpot
+ */
 class Gravity_Flow_Step_Feed_HubSpot extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'hubspot';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = '\BigSea\GFHubSpot\GF_HubSpot';
+
+	/**
+	 * The slug used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_slug = 'gravityforms-hubspot';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'HubSpot';
 	}
 
+	/**
+	 * Returns the class name for the add-on.
+	 *
+	 * @return string
+	 */
 	public function get_feed_add_on_class_name() {
 		if ( ! class_exists( '\BigSea\GFHubSpot\GF_HubSpot' ) ) {
 			$this->_class_name = 'GF_HubSpot';
