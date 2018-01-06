59fa93e7178866c5b52f051e5763dc18c2d356f6
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:52:43 +0000

    Fixed Gravity_Flow_Step_Feed_MailChimp docs

--- includes/steps/class-step-feed-mailchimp.php
+++ includes/steps/class-step-feed-mailchimp.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed MailChimp
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_MailChimp
  * @copyright   Copyright (c) 2015-2018, Steven Henty S.L.
@@ -14,15 +13,39 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_MailChimp
+ */
 class Gravity_Flow_Step_Feed_MailChimp extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'mailchimp';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFMailChimp';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'MailChimp';
 	}
 
+	/**
+	 * Returns the URL for the step icon.
+	 *
+	 * @return string
+	 */
 	public function get_icon_url() {
 		return $this->get_base_url() . '/images/mailchimp.svg';
 	}
