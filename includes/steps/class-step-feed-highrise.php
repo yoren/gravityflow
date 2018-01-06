0f7d48312d28db90ffdde50c4cfc70e59fd4b6e2
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:41:40 +0000

    Fixed Gravity_Flow_Step_Feed_Highrise docs

--- includes/steps/class-step-feed-highrise.php
+++ includes/steps/class-step-feed-highrise.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Highrise
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_Highrise
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,15 +13,41 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_Highrise
+ */
 class Gravity_Flow_Step_Feed_Highrise extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'highrise';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFHighrise';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Highrise';
 	}
 
+	/**
+	 * Returns the feed name.
+	 *
+	 * @param array $feed The Highrise feed properties.
+	 *
+	 * @return string
+	 */
 	public function get_feed_label( $feed ) {
 		$label = $feed['meta']['feed_name'];
 
