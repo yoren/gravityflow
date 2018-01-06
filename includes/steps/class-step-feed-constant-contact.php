c5be3d33fbf5c4ec23e5f3c1aa0fb0090bfb650d
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:22:45 +0000

    Fixed Gravity_Flow_Step_Feed_Constant_Contact docs

--- includes/steps/class-step-feed-constant-contact.php
+++ includes/steps/class-step-feed-constant-contact.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Constant Contact
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_Constant_Contact
  * @copyright   Copyright (c) 2017, Steven Henty S.L.
@@ -14,15 +13,48 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_Constant_Contact
+ */
 class Gravity_Flow_Step_Feed_Constant_Contact extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'constant_contact';
+
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GF_Constant_Contact';
+
+	/**
+	 * The slug used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_slug = 'gravity-forms-constant-contact';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Constant Contact';
 	}
 
+	/**
+	 * Returns the feed name.
+	 *
+	 * @param array $feed The Constant Contact feed properties.
+	 *
+	 * @return string
+	 */
 	public function get_feed_label( $feed ) {
 		$label = $feed['meta']['feed_name'];
 
