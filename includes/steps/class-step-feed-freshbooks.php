ce2cdf5a366b0615d1e411920502410783ccac36
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:36:55 +0000

    Fixed Gravity_Flow_Step_Feed_FreshBooks docs

--- includes/steps/class-step-feed-freshbooks.php
+++ includes/steps/class-step-feed-freshbooks.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed FreshBooks
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_FreshBooks
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,11 +13,30 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_FreshBooks
+ */
 class Gravity_Flow_Step_Feed_FreshBooks extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'freshbooks';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFFreshBooks';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'FreshBooks';
 	}
