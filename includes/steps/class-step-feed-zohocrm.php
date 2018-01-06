9108fdaff86d96ba924a384c83997eac2c073a2f
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 16:26:57 +0000

    Fixed Gravity_Flow_Step_Feed_ZohoCRM docs

--- includes/steps/class-step-feed-zohocrm.php
+++ includes/steps/class-step-feed-zohocrm.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Zoho CRM
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_ZohoCRM
  * @copyright   Copyright (c) 2015-2018, Steven Henty S.L.
@@ -14,11 +13,30 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_ZohoCRM
+ */
 class Gravity_Flow_Step_Feed_ZohoCRM extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'zohocrm';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFZohoCRM';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Zoho CRM';
 	}
