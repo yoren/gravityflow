8adfc7853156f03071d712424dd44e2f5e9ebcaf
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:17:07 +0000

    Fixed Gravity_Flow_Step_Feed_Campfire docs

--- includes/steps/class-step-feed-campfire.php
+++ includes/steps/class-step-feed-campfire.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Campfire
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_Campfire
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,11 +13,30 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_Campfire
+ */
 class Gravity_Flow_Step_Feed_Campfire extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'campfire';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFCampfire';
 
+	/**
+	 * Returns the step label.c
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Campfire';
 	}
