f3fe111c579a3a173ca80928650e972881aeaa82
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:39:58 +0000

    Fixed Gravity_Flow_Step_Feed_HelpScout docs

--- includes/steps/class-step-feed-helpscout.php
+++ includes/steps/class-step-feed-helpscout.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Help Scout
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_HelpScout
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,21 +13,52 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_HelpScout
+ */
 class Gravity_Flow_Step_Feed_HelpScout extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'helpscout';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFHelpScout';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Help Scout';
 	}
 
+	/**
+	 * Returns the feed name.
+	 *
+	 * @param array $feed The Help Scout feed properties.
+	 *
+	 * @return string
+	 */
 	public function get_feed_label( $feed ) {
 		$label = $feed['meta']['feed_name'];
 
 		return $label;
 	}
 
+	/**
+	 * Returns the URL for the step icon.
+	 *
+	 * @return string
+	 */
 	public function get_icon_url() {
 		return $this->get_base_url() . '/images/helpscout-icon.png';
 	}
