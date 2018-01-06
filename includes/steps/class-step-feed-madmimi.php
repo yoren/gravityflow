2fa2fdbdbb7c9297e859d81282d205248f11bd92
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:48:20 +0000

    Fixed Gravity_Flow_Step_Feed_MadMimi docs

--- includes/steps/class-step-feed-madmimi.php
+++ includes/steps/class-step-feed-madmimi.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Mad Mimi
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_MadMimi
  * @copyright   Copyright (c) 2015-2018, Steven Henty S.L.
@@ -14,15 +13,41 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_MadMimi
+ */
 class Gravity_Flow_Step_Feed_MadMimi extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'madmimi';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'GFMadMimi';
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Mad Mimi';
 	}
 
+	/**
+	 * Returns the feed name.
+	 *
+	 * @param array $feed The Mad Mimi feed properties.
+	 *
+	 * @return string
+	 */
 	public function get_feed_label( $feed ) {
 		$label = $feed['meta']['feed_name'];
 
