4953f1eaf3c3c106c95619cc4b2e1552c50ef76f
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-01-06 15:52:32 +0000

    Fixed Gravity_Flow_Step_Feed_Pipedrive docs

--- includes/steps/class-step-feed-pipedrive.php
+++ includes/steps/class-step-feed-pipedrive.php
@@ -2,7 +2,6 @@
 /**
  * Gravity Flow Step Feed Pipedrive
  *
- *
  * @package     GravityFlow
  * @subpackage  Classes/Gravity_Flow_Step_Feed_Pipedrive
  * @copyright   Copyright (c) 2016-2018, Steven Henty S.L.
@@ -14,13 +13,44 @@ if ( ! class_exists( 'GFForms' ) ) {
 	die();
 }
 
+/**
+ * Class Gravity_Flow_Step_Feed_Pipedrive
+ */
 class Gravity_Flow_Step_Feed_Pipedrive extends Gravity_Flow_Step_Feed_Add_On {
+
+	/**
+	 * The step type.
+	 *
+	 * @var string
+	 */
 	public $_step_type = 'pipedrive';
 
+	/**
+	 * The name of the class used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_class_name = 'WPGravityFormsToPipeDriveCRM';
+
+	/**
+	 * The slug used by the add-on.
+	 *
+	 * @var string
+	 */
 	protected $_slug = 'gf2pdcrm';
+
+	/**
+	 * The current instance of the Pipedrive add-on or null.
+	 *
+	 * @var null|WPGravityFormsToPipedriveAddOn
+	 */
 	protected $_addon_instance = null;
 
+	/**
+	 * Returns the step label.
+	 *
+	 * @return string
+	 */
 	public function get_label() {
 		return 'Pipedrive';
 	}
@@ -28,7 +58,7 @@ class Gravity_Flow_Step_Feed_Pipedrive extends Gravity_Flow_Step_Feed_Add_On {
 	/**
 	 * Retrieve an instance of the add-on associated with this step.
 	 *
-	 * @return GFFeedAddOn
+	 * @return WPGravityFormsToPipedriveAddOn|null
 	 */
 	public function get_add_on_instance() {
 		if ( ! is_object( $this->_add_on_instance ) ) {
