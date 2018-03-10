9a46190e488e95ae7ef921d7da8102e59a5a73d4
Yoren Chang <yorenchang@gmail.com>
2018-03-10 20:07:09 +0800

    Added assignee search feature

--- js/form-settings.js
+++ js/form-settings.js
@@ -4,7 +4,43 @@
 
 	$(document).ready(function () {
 
-		$('#assignees, #editable_fields, #workflow_notification_users, .gravityflow-multiselect-ui').multiSelect();
+		$('#editable_fields, #workflow_notification_users, .gravityflow-multiselect-ui').multiSelect();
+
+        $('#assignees').multiSelect({
+            selectableHeader: "<input type='text' class='search-input' autocomplete='off' placeholder='Type to search'>",
+            selectionHeader: "<input type='text' class='search-input' autocomplete='off' placeholder='Type to search'>",
+            afterInit: function(ms){
+                var that = this,
+                    $selectableSearch = that.$selectableUl.prev(),
+                    $selectionSearch = that.$selectionUl.prev(),
+                    selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)',
+                    selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
+
+                that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
+                    .on('keydown', function(e){
+                        if (e.which === 40){
+                            that.$selectableUl.focus();
+                            return false;
+                        }
+                    });
+
+                that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
+                    .on('keydown', function(e){
+                        if (e.which == 40){
+                            that.$selectionUl.focus();
+                            return false;
+                        }
+                    });
+            },
+            afterSelect: function(){
+                this.qs1.cache();
+                this.qs2.cache();
+            },
+            afterDeselect: function(){
+                this.qs1.cache();
+                this.qs2.cache();
+            }
+        });
 
 		var gravityFlowIsDirty = false, gravityFlowSubmitted = false;
 
